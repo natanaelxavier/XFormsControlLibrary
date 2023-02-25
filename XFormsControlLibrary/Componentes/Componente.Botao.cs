af30913336bdee543f899e0b4fedccd31200be2a
natanael <natanael@iskisita.com.br>
2023-02-25 10:57:09 -0300

    Criando uma nova classe de botão. Melhorado e com mais parametros para modificar.

--- XFormsControlLibrary/Componentes/Componente.Botao.cs
+++ XFormsControlLibrary/Componentes/Componente.Botao.cs
@@ -16,75 +16,138 @@ namespace XFormsControlLibrary.Componentes
     public class ButtonXForms : Button
     {
         #region Atributos
-        private Entidades.Enums.EBordaTipo eTipoBorda = Entidades.Enums.EBordaTipo.Nenhum;
+        private int borderSize = 0;
+        private int borderRadius = 20;
+        private Color borderColor = Color.PaleVioletRed;
         #endregion
 
         #region Propriedades Visual Studio
         [Category("XForms")]
-        public Entidades.Enums.EBordaTipo TipoBorda
+        public int BorderSize
         {
-            get
-            {
-                return eTipoBorda;
-            }
+            get { return borderSize; }
             set
             {
-                eTipoBorda = value;
+                borderSize = value;
                 this.Invalidate();
             }
+        }
+        [Category("XForms")]
+        public int BorderRadius
+        {
+            get { return borderRadius; }
+            set
+            {
+                borderRadius = value;
+                this.Invalidate();
+            }
+        }
+        [Category("XForms")]
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+        [Category("XForms")]
+        public Color BackgroundColor
+        {
+            get { return this.BackColor; }
+            set { this.BackColor = value; }
+        }
+        [Category("XForms")]
+        public Color TextColor
+        {
+            get { return this.ForeColor; }
+            set { this.ForeColor = value; }
         }
         #endregion
 
         #region Construtores / Inicializadores
         public ButtonXForms()
         {
-            this.Paint += BotaoXForms_Paint;
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.Size = new Size(150, 40);
+            this.BackColor = Color.MediumSlateBlue;
+            this.ForeColor = Color.White;
+            this.Resize += new EventHandler(Button_Resize);
         }
         #endregion
 
         #region Metodos/Eventos
-        private void BotaoXForms_Paint(object sender, PaintEventArgs e)
+        private void Button_Resize(object sender, EventArgs e)
         {
-            try
+            if (borderRadius > this.Height)
+                borderRadius = this.Height;
+        }
+        private GraphicsPath GetFigurePath(Rectangle rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            Rectangle rectSurface = this.ClientRectangle;
+            Rectangle rectBorder = Rectangle.Inflate(rectSurface, -borderSize, -borderSize);
+            int smoothSize = 2;
+            if (borderSize > 0)
+                smoothSize = borderSize;
+            if (borderRadius > 2) //Rounded button
             {
-                Button btn = (Button)sender;
-                GraphicsPath buttonPath = new GraphicsPath();
-                Rectangle newRectangle = btn.ClientRectangle;
-                newRectangle.Inflate(-3, -3);
-
-                switch (TipoBorda)
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - borderSize))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, smoothSize))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
                 {
-                    case Entidades.Enums.EBordaTipo.Completo:
-                        buttonPath.AddArc(new Rectangle(newRectangle.X, newRectangle.Y, 10, 10), 180, 90);
-                        buttonPath.AddArc(new Rectangle(newRectangle.Width - 10 + newRectangle.X, newRectangle.Y, 10, 10), 270, 90);
-                        buttonPath.AddArc(new Rectangle(newRectangle.Width - 10 + newRectangle.X, newRectangle.Height - 10 + newRectangle.Y, 10, 10), 0, 90);
-                        buttonPath.AddArc(new Rectangle(newRectangle.X, newRectangle.Height - 10 + newRectangle.Y, 10, 10), 90, 90);
-                        break;
-                    case Entidades.Enums.EBordaTipo.Esquerda:
-                        buttonPath.AddArc(new Rectangle(newRectangle.X, newRectangle.Y, 10, 10), 180, 90);
-                        buttonPath.AddLine(new Point(newRectangle.Width, 2), new Point(newRectangle.Width - newRectangle.X, newRectangle.Y));
-                        buttonPath.AddLine(new Point(newRectangle.Width, newRectangle.Y), new Point(newRectangle.Width, newRectangle.Height - newRectangle.Y + 3));
-                        buttonPath.AddArc(new Rectangle(newRectangle.X, newRectangle.Height - 12 + newRectangle.Y, 10, 10), 90, 90);
-                        break;
-                    case Entidades.Enums.EBordaTipo.Direita:
-                        buttonPath.AddLine(new Point(newRectangle.X, newRectangle.Y), new Point(2, 2));
-                        buttonPath.AddArc(new Rectangle(newRectangle.Width - 10 + newRectangle.X, newRectangle.Y, 10, 10), 270, 90);
-                        buttonPath.AddArc(new Rectangle(newRectangle.Width - 10 + newRectangle.X, newRectangle.Height - 10 + newRectangle.Y, 10, 10), 0, 90);
-                        buttonPath.AddLine(new Point(newRectangle.X, newRectangle.Height + 2), new Point(2, 2));
-                        break;
-                    default:
-                        buttonPath.AddLine(new Point(newRectangle.X, newRectangle.Y), new Point(newRectangle.Width + newRectangle.X, newRectangle.Y));
-                        buttonPath.AddLine(new Point(newRectangle.Width + newRectangle.X, newRectangle.Height + newRectangle.Y), new Point(newRectangle.X, newRectangle.Height + newRectangle.Y));
-                        break;
+                    pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    //Button surface
+                    this.Region = new Region(pathSurface);
+                    //Draw surface border for HD result
+                    pevent.Graphics.DrawPath(penSurface, pathSurface);
+                    //Button border                    
+                    if (borderSize >= 1)
+                        //Draw control border
+                        pevent.Graphics.DrawPath(penBorder, pathBorder);
                 }
-                buttonPath.CloseFigure();
-                btn.Region = new Region(buttonPath);
             }
-            catch (Exception Problema)
+            else //Normal button
             {
-                MessageBox.Show(Problema.Message);
+                pevent.Graphics.SmoothingMode = SmoothingMode.None;
+                //Button surface
+                this.Region = new Region(rectSurface);
+                //Button border
+                if (borderSize >= 1)
+                {
+                    using (Pen penBorder = new Pen(borderColor, borderSize))
+                    {
+                        penBorder.Alignment = PenAlignment.Inset;
+                        pevent.Graphics.DrawRectangle(penBorder, 0, 0, this.Width - 1, this.Height - 1);
+                    }
+                }
             }
         }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            this.Parent.BackColorChanged += new EventHandler(Container_BackColorChanged);
+        }
+        private void Container_BackColorChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
         #endregion
+
     }
 }
